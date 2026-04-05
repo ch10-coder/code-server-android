f81d82c9ddc45c1ff9506f197e65dd9980e0ec6b
ch10-coder <ch10-coder@users.noreply.github.com>
2026-04-05 05:38:36 +0530

    initial commit: Android wrapper for code-server

---
+++ app/src/main/java/com/ch10coder/codeserver/MainActivity.java
@@ -0,0 +1,47 @@
+package com.ch10coder.codeserver;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Use a simple layout with just a WebView
+        myWebView = new WebView(this);
+        setContentView(myWebView);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        
+        // This makes it look better on phones
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+
+        myWebView.setWebViewClient(new WebViewClient());
+
+        // LOAD YOUR CODE-SERVER HERE!
+        // We'll use localhost because that's where code-server runs
+        myWebView.loadUrl("http://localhost:8080");
+    }
+
+    // This handles the "Back" button so it doesn't just exit the app
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
